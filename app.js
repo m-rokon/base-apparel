c055748335636724252b4d220d87936ec4854ebf
Mohammad Al Mamun Rokon <rokon@Rokons-MBP.local>
2022-12-14 02:48:57 +0600

    cleaned & `JS` updated

--- app.js
+++ app.js
@@ -3,15 +3,23 @@ const submitBtn = document.querySelector(".submit_btn");
 const errorIcon = document.querySelector(".error_icon");
 const input = document.querySelector("#email");
 
-// const form = document.querySelector("form");
-
 // validate input
 const validateEmail = () => {
-    let validRegex =
+    const validRegex =
         /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    input.addEventListener("keyup", () => {
+        if (input.value === "") {
+            message.textContent = "";
+            errorIcon.classList.add("hidden");
+        }
+    });
     if (input.value != "" && input.value.match(validRegex)) {
         message.textContent = "Thanks for submitting the form.";
         errorIcon.classList.add("hidden");
+        setTimeout(() => {
+            message.textContent = "";
+            input.value = "";
+        }, 2000);
     } else {
         errorIcon.classList.remove("hidden");
         message.textContent = "Enter a valid email address";
